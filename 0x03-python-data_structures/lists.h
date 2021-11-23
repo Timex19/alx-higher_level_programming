0936b513d09379efa8894703c7514ea2f84ca710
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-11-23 11:19:25 +0100

    Change the logic of the program because the logic was so bad

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,7 +7,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Alx project
  */
 typedef struct listint_s
 {
@@ -18,7 +18,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int is_palindrome(listint_t **head);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
