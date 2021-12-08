65e362a05a6835e8510edcad462fb3039b7b3e53
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-12-08 12:24:39 +0100

    Fix - Missing self param in circumference method

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
+""" Class form python bytecode for magic objects"""
 import math
 
 
 class MagicClass:
-
-    """Class that stores the properties
-    of a circumference"""
+    """Class constructor"""
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    """ Method that calculates the area of the circumference """
+    """Return the area"""
     def area(self):
-        return ((self.__radius ** 2) * math.pi)
+        return self.__radius ** 2 * math.pi
 
-    """ Method that calculates the perimeter of a circumference """
+    """Return circumference"""
     def circumference(self):
-        return (2 * math.pi * self.__radius)
+        return 2 * math.pi * self.__radius
