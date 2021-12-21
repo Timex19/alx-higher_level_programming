8978f47601db2f33316159bb51d6756c4b745516
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-12-21 13:58:21 +0100

    Feature - adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-'''
-file: 9-add_item.py
-description: adds all arguments to a Python list, and then save them to a file
-'''
-from sys import argv
-from os import path
+""" Module that adds all arguments to a Python list, and then
+save them to a file
+"""
+import sys
+import os.path
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
 
-arg_list = []
-if path.isfile(filename):
-    arg_list = load_from_json_file(filename)
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = load_file("add_item.json")
 
-for a in argv[1:]:
-    arg_list.append(a)
+for arg in sys.argv[1:]:
+    my_list.append(arg)
 
-save_to_json_file(arg_list, "add_item.json")
+save_file(my_list, "add_item.json")
