08b50d5ee705eafebb8299074b999dc1a7b9da18
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-05 20:51:17 +0100

    [Fix]: Typo

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-'''
-Lists all State objects, and corresponding City objects
-'''
-
-
-from sys import argv
+"""
+Lists all City objects from the database hbtn_0e_101_usa
+"""
+import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
@@ -12,14 +10,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-        engine = create_engine(
-                    'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-            Base.metadata.create_all(engine)
-                InstanceSession = sessionmaker(bind=engine)
-                    session = InstanceSession()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-                        cities = session.query(City).order_by(City.id).all()
-                            for city in cities:
-                                        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                                            session.close()
+    st = session.query(State).join(City).order_by(City.id).all()
+
+    for state in st:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
