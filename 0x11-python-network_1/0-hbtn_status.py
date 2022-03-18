4db13eedc81dd75f3d1424c2f1fa30ec8906e1f0
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-18 10:45:15 +0100

    [Fix]: print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 import urllib.request
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
