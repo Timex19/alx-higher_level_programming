a08146650df9e4d4cb65e3e6a45c193823646ade
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-18 10:46:51 +0100

    [Fix]: print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
