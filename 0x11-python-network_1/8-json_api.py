ebd89d8f7c0acffb33f21a752e97b76ac85f2e01
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-18 11:25:08 +0100

    [Feature]: adding a script that sends a POST request with a letter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,20 +8,20 @@ import sys
 
 
 if __name__ == "__main__":
-        data = {'q': ""}
+    data = {'q': ""}
 
-            try:
-                        data['q'] = sys.argv[1]
-            except:
-                        pass
+    try:
+        data['q'] = sys.argv[1]
+    except:
+        pass
 
-                        r = requests.post('http://0.0.0.0:5000/search_user', data)
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
 
-                            try:
-                                        json_o = r.json()
-                                                if not json_o:
-                                                                print("No result")
-                                                else:
-                                                                print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
-                            except:
-                                        print("Not a valid JSON")
+    try:
+        json_o = r.json()
+        if not json_o:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+    except:
+        print("Not a valid JSON")
