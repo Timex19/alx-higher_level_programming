21dbb04b00c2720b33246acfdae4223ba435e892
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-12 21:45:54 +0100

    [Feature]: adding script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-    if (contents === undefined) {
-	console.log(err);
-    } else {
-	console.log(contents);
-    }
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  console.log(error || data);
 });
