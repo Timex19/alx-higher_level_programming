25c6ee8f5b7c7b8799db26cc0b83cc2658dacacb
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-13 17:08:41 +0100

    Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
 request(url, function (err, response, body) {
   if (err) { console.error(err); }
   const obj = JSON.parse(body);
