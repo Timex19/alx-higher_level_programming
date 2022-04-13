8356c78ba42d5c96e018ba3898f181322e3ffd8c
Ojo Timilehin Joseph <ojooluwatimilehipeter@yahoo.com>
2022-04-13 16:05:49 +0000

    Feature - Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+    if (err) { console.error(err); }
+    const obj = JSON.parse(body);
+    console.log(obj.title);
+});
