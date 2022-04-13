173f773176a59876021ee15caa657278299367e1
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-13 17:04:12 +0100

    Fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,6 @@
-
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-
-request(url)
-    .on('response', function (response) {
-	console.log(`code: ${response.statusCode}`);
-    });
+request(process.argv[2], function (err, response) {
+  if (err) { console.error(err); }
+  console.log('code:', response && response.statusCode);
+});
